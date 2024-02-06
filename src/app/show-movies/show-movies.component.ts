f0cb5a6b0fe6b3513c2791b4a253820ac46b7e90
dovakin0007 <dovakin0007@gmail.com>
2024-02-06 22:21:11 +0530

    Changed to handle file path

--- src/app/show-movies/show-movies.component.ts
+++ src/app/show-movies/show-movies.component.ts
@@ -1,16 +1,62 @@
+// import { Component, OnInit } from '@angular/core';
+// // import { PostService } from '../post.service';
+// import { NgFor } from '@angular/common';
+// import {HttpClient} from "@angular/common/http"
+
+// interface Movie {
+//   name?: string;
+//   actor?: string;
+//   imageType?: string;
+//   base64String?: string;
+//   imagePath?: string;
+//   imagePathAsset?: string;
+
+// }
+// @Component({
+//   selector: 'app-show-movies',
+//   standalone: true,
+//   imports: [NgFor],
+//   templateUrl: './show-movies.component.html',
+//   styleUrl: './show-movies.component.css'
+// })
+// export class ShowMoviesComponent {
+//   private getUrl = 'http://localhost:8080/api/movies'
+//   json_data:any = [];
+//   movie_data:any;
+//   constructor (private http:HttpClient) {
+
+//   }
+
+
+  
+// showConfig() {
+//   this.http.get(this.getUrl).subscribe((data) =>{
+//     this.json_data = {
+//       name: data.
+//     };
+//     console.log(this.json_data);
+//    }
+//   );
+// }
+// ngOnInit() {
+//   this.showConfig()
+// }
+// }
+
+
 import { Component, OnInit } from '@angular/core';
-// import { PostService } from '../post.service';
 import { NgFor } from '@angular/common';
-import {HttpClient} from "@angular/common/http"
-import {Observable} from "rxjs";
+import { HttpClient } from "@angular/common/http";
 
 interface Movie {
   name?: string;
   actor?: string;
   imageType?: string;
   base64String?: string;
-
+  imagePath?: string;
+  imagePathAsset?: string;
 }
+
 @Component({
   selector: 'app-show-movies',
   standalone: true,
@@ -19,23 +65,23 @@ interface Movie {
   styleUrl: './show-movies.component.css'
 })
 export class ShowMoviesComponent {
-  private getUrl = 'http://localhost:8080/api/movies'
-  json_data:any = [];
-  movie_data:any;
-  constructor (private http:HttpClient) {
+  private getUrl = 'http://localhost:8080/api/movies';
+  json_data: any = [];
+  movie_data: any;
 
+  constructor(private http: HttpClient) { }
+
+  showConfig() {
+    this.http.get<Movie[]>(this.getUrl).subscribe((data: Movie[]) => {
+      this.json_data = data.map(movie => ({
+        ...movie,
+        imagePathAsset: movie.imagePathAsset?.replace('/C:\\angular lol\\my_app\\src\\assets\\/g', "../../assets/")
+      }));
+      console.log(this.json_data);
+    });
   }
 
-
-  
-showConfig() {
-  this.http.get(this.getUrl).subscribe((data) =>{
-    this.json_data = data;
-    console.log(this.json_data);
-   }
-  );
-}
-ngOnInit() {
-  this.showConfig()
-}
+  ngOnInit() {
+    this.showConfig();
+  }
 }
